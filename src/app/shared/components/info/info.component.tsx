0dd1a78bffe5e8f0510cd7fa9a4cf92950160b3a
audrey <audrey@devzone.fr>
2024-04-24 11:05:45 +0200

    fix: use Image in info component

--- src/app/shared/components/info/info.component.tsx
+++ src/app/shared/components/info/info.component.tsx
@@ -1,12 +1,13 @@
 import './info.scss'
 import {InfoComponentProps} from '@/app/shared/interfaces/props.interface';
+import Image from 'next/image';
 
 
 export default function InfoComponent({ icon, text, value }: InfoComponentProps) {
 
     return <div className={'info'}>
         <div className={'info-picto'}>
-            <img src={icon.default.src} alt={'icon'} />
+            <Image src={icon.default.src} alt={'icon'} width={60} height={60} />
         </div>
         <div className={'info-text'}>
             <h3>{value}</h3>
